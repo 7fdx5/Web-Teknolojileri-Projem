d80a6cb55917c26cb824f5d47d53d424729883c2
7fdx5 <alialdous822@gmail.com>
2022-05-15 20:14:06 +0300

    ilk guncel

---
+++ kisi.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/contact.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+    <?php echo "merhba :".$_POST["text"];?>
+
+<section class="contact-me">
+            <div class="content">
+                <div class="container-texts">
+                    <h4>Merhaba, benim adım </h4>
+                    <h1>Ali <span>Al Dous</span></h1>
+                    <h3> Ben <span>Bilgisayar </span>Mühendisiyim </h3>
+                    <p>Bunlar benimle ve kişisel hesaplarımla iletişim kurmanın yolları</p>
+                    <div class=" social">
+                        <a href="https://www.instagram.com/7fdx5/" target="_blank"><img src="imgs/icon2.png" alt="instagram"></a>
+                        <a href="https://www.facebook.com/profile.php?id=100044624404419" target="_blank"><img src="imgs/icon3.png" alt="facebook"></a>
+                        <a href="https://twitter.com/3laip" target="_blank"><img src="imgs/hcon4.png" alt="twitter"></a>
+
+
+                    </div>
+
+                </div>
+    
+    
+</body>
+</html>
